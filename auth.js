dcd05bf0f1ac87ff72efd91962cd05b350cb7976
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-26 16:08:38 -0400

    13 of 13

---
+++ auth.js
@@ -0,0 +1,38 @@
+const Hapi = require('@hapi/hapi');
+const Auth = require('@hapi/basic');
+
+const user = { name: 'hapi', password: 'auth' };
+
+const validate = async (request, username, password, h) => {
+  const isValid = username === user.name && password === user.password;
+
+  return { isValid, credentials: { name: user.name } };
+};
+
+const init = async () => {
+  try {
+    const server = Hapi.Server({
+      host: 'localhost',
+      port: Number(process.argv[2] || 8080)
+    });
+
+    await server.register(Auth);
+
+    server.auth.strategy('simple', 'basic', { validate });
+    server.auth.default('simple');
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, h) {
+        return 'welcome';
+      }
+    });
+
+    await server.start();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
